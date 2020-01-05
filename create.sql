08f2edd84cf69a06159081204237e2576d69d9df
Fynn Michell-Auli <f.michell-auli@live.de>
2020-01-05 20:24:42 +0100

    Speedtest

---
+++ create.sql
@@ -0,0 +1,68 @@
+CREATE TABLE countrys (
+	id SERIAL PRIMARY KEY,
+	name TEXT UNIQUE,
+	cc TEXT UNIQUE
+);
+
+
+CREATE TABLE servers (
+	id INT PRIMARY KEY,		-- ID aus json
+	url TEXT,
+	url2 TEXT,
+	lat FLOAT,
+	lon FLOAT,
+	name TEXT,
+	country_id INT,
+	sponsor TEXT,
+	host TEXT,
+	d FLOAT,
+
+	FOREIGN KEY(country_id) REFERENCES countrys(id)
+); 
+
+
+CREATE TABLE clients (
+	id SERIAL PRIMARY KEY,
+	ip TEXT,
+	lat FLOAT,
+	lon FLOAT,
+	isp TEXT,
+	isprating FLOAT,
+	rating INT,
+	ispdlavg INT,
+	ispulavg INT,
+	loggedin INT,
+	country_id INT,
+	
+	FOREIGN KEY(country_id) REFERENCES countrys(id)
+);
+
+
+CREATE TABLE speed_test (
+	id SERIAL PRIMARY KEY,
+	time TIMESTAMP,
+	download_speed FLOAT,
+	upload_speed FLOAT,
+	ping FLOAT,
+	server_id INT,
+	latency FLOAT,
+	bytes_sent INT,
+	bytes_received INT,
+	client_id INT,
+
+	FOREIGN KEY(server_id) REFERENCES servers(id),
+	FOREIGN KEY(client_id) REFERENCES clients(id)
+);
+	
+
+
+-- Tabelle mit Rohdaten
+CREATE TABLE rawjson (
+	id SERIAL PRIMARY KEY,
+	jsondata json
+);
+
+CREATE VIEW last_speedtest AS (
+SELECT *
+FROM rawjson
+WHERE (jsondata->>'timestamp')::timestamp = (SELECT MAX(rd.jsondata->>'timestamp')::timestamp FROM rawjson rd));
---
+++ insertscript.sql
@@ -0,0 +1,92 @@
+-- JSON Rohdaten importieren
+\set import `speedtest-cli --json --secure`
+INSERT INTO rawjson(jsondata) VALUES(:'import');
+
+
+
+-- Insert and parse JSON Data to country
+INSERT INTO countrys (name, cc)
+SELECT jsondata->'server'->>'country',
+   	    jsondata->'server'->>'cc'
+FROM last_speedtest
+WHERE NOT EXISTS (
+			   	  SELECT 1
+			   	  FROM countrys c 
+			   	  WHERE c.name = (SELECT ls.jsondata->'server'->>'country' FROM last_speedtest ls)
+			   	  AND c.cc     = (SELECT ls.jsondata->'server'->>'cc' FROM last_speedtest ls));
+
+
+
+-- Insert and parse JSON Data to server
+INSERT INTO servers (id, url, url2, lat, lon, name, country_id, sponsor, host, d)
+SELECT (jsondata->'server'->>'id')::int,
+	   (jsondata->'server'->>'url')::text,
+	   (jsondata->'server'->>'url2')::text,
+	   (jsondata->'server'->>'lat')::float,
+	   (jsondata->'server'->>'lon')::float,
+	   (jsondata->'server'->>'name')::text,
+	   (SELECT c.id FROM countrys c WHERE c.name = (jsondata->'server'->>'country')::text),
+	   (jsondata->'server'->>'sponsor')::text,
+	   (jsondata->'server'->>'host')::text,
+	   (jsondata->'server'->>'d')::float
+	 
+FROM last_speedtest
+WHERE NOT EXISTS (
+				  	SELECT 1
+				  	FROM servers s
+				  	WHERE s.id = (SELECT (ls.jsondata->'server'->>'id')::int FROM last_speedtest ls));
+
+
+
+-- Insert and parse JSON Data to client
+INSERT INTO clients (ip, lat, lon, isp, isprating, rating, ispdlavg, ispulavg, loggedin, country_id)     
+SELECT (jsondata->'client'->>'ip')::text,
+       (jsondata->'client'->>'lat')::float,
+       (jsondata->'client'->>'lon')::float,
+       (jsondata->'client'->>'isp')::text,
+       (jsondata->'client'->>'isprating')::float,
+       (jsondata->'client'->>'rating')::int,
+       (jsondata->'client'->>'ispdlavg')::int,
+       (jsondata->'client'->>'ispulavg'):: int,
+       (jsondata->'client'->>'loggedin'):: int,
+	   (SELECT co.id FROM countrys co WHERE co.cc = (jsondata->'client'->>'country')::text)
+FROM last_speedtest
+WHERE NOT EXISTS (
+					SELECT 1
+					FROM clients cl
+					WHERE cl.ip         = (jsondata->'client'->>'ip')::text
+					  AND cl.lat        = (jsondata->'client'->>'lat')::float
+				      AND cl.lon        = (jsondata->'client'->>'lon')::float 
+					  AND cl.isp 	    = (jsondata->'client'->>'isp')::text
+				      AND cl.isprating  = (jsondata->'client'->>'isprating')::float 
+					  AND cl.rating     = (jsondata->'client'->>'rating')::int 
+					  AND cl.ispdlavg   = (jsondata->'client'->>'ispdlavg')::int 
+					  AND cl.ispulavg   = (jsondata->'client'->>'ispulavg')::int 
+					  AND cl.loggedin   = (jsondata->'client'->>'loggedin')::int 
+				      AND cl.country_id = (SELECT co.id FROM countrys co WHERE co.cc = (jsondata->'client'->>'country')::text));
+
+
+
+-- Insert and parse JSON Data to speed_test
+INSERT INTO speed_test(time, download_speed, upload_speed, ping, server_id, latency, bytes_sent, bytes_received, client_id)
+SELECT (jsondata->>'timestamp')::timestamp,
+	   (jsondata->>'download')::float,
+	   (jsondata->>'upload')::float,
+	   (jsondata->>'ping')::float,
+	   (jsondata->'server'->>'id')::int,
+	   (jsondata->'server'->>'latency')::float,
+	   (jsondata->>'bytes_sent')::int,
+	   (jsondata->>'bytes_received')::int,
+	   (SELECT cl.id
+		  FROM clients cl
+     	 WHERE cl.ip         = (jsondata->'client'->>'ip')::text
+		   AND cl.lat        = (jsondata->'client'->>'lat')::float
+		   AND cl.lon        = (jsondata->'client'->>'lon')::float 
+		   AND cl.isp 	    = (jsondata->'client'->>'isp')::text
+		   AND cl.isprating  = (jsondata->'client'->>'isprating')::float 
+		   AND cl.rating     = (jsondata->'client'->>'rating')::int 
+		   AND cl.ispdlavg   = (jsondata->'client'->>'ispdlavg')::int 
+		   AND cl.ispulavg   = (jsondata->'client'->>'ispulavg')::int 
+		   AND cl.loggedin   = (jsondata->'client'->>'loggedin')::int 
+		   AND cl.country_id = (SELECT co.id FROM countrys co WHERE co.cc = (jsondata->'client'->>'country')::text))
+FROM last_speedtest;
